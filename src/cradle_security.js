4685de89dca3a3e2a2b9f67865a34e3b703ddc65
Marcello Gesmundo <mgesmundo@yoovant.com>
2014-07-08 08:30:56 +0200

    Bump version

--- src/cradle_security.js
+++ src/cradle_security.js
@@ -5,7 +5,7 @@
  *
  * # License
  *
- * Copyright (c) 2012-2013 Yoovant by Marcello Gesmundo. All rights reserved.
+ * Copyright (c) 2012-2014 Yoovant by Marcello Gesmundo. All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
